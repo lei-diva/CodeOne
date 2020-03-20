949610216559019666e5e62a36243de06f267bc2
lei-diva <divaaleii@gmail.com>
2020-03-20 04:38:29 -0700

    remove test landing page route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {SignInPage} from './pages/SignIn/SignIn';
 import './App.css';
 import {SignUpPage} from './pages/SignUp/SignUp';
 import ProfilePage from './pages/Profile/Profile';
-import { LandingPage } from './pages/Landing/Landing';
 
 class App extends React.Component {
     constructor() {
@@ -61,7 +60,6 @@ class App extends React.Component {
             <Switch>
         <Route exact path='/playground' component={() => <Playground userRef={this.state.userRef} currentUser={this.state.currentUser}/>}/>
             <Route exact path='/' component={() => <Home currentUser={this.state.currentUser}/>} />
-            <Route exact path='/landing' component={LandingPage} />
             <Route exact path='/login' component={SignInPage} />
             <Route exact path='/sign-up' component={SignUpPage} />
             <Route exact path='/profile' component={() => <ProfilePage currentUser={this.state.currentUser} userRef={this.state.userRef}/>} />
